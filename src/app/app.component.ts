11765bbbc78ee0e177bd3611ba0ee53c3002e836
Shaw Coney <rockconey@gmail.com>
2020-04-05 15:48:12 -0500

    studio completed no bonus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,11 @@ import { JsonPipe } from '@angular/common';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
    window.fetch(satellitesUrl).then(function(response) {
@@ -25,6 +27,7 @@ export class AppComponent {
            let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].LaunchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             
            this.sourceList.push(satellite);
+           this.displayList = this.sourceList.slice(0);
 
           }
         }.bind(this));  
@@ -33,6 +36,18 @@ export class AppComponent {
        
       }.bind(this));
     
+  }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for (let i = 0; i <this.sourceList.length; i++) {
+      let name = this.sourceList[i].name.toLowerCase();
+      if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+      }
+    }
+    this.displayList = matchingSatellites;
   }
 }
 
